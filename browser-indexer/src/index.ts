2b468494dd2525b35066ae43937b1506d6f00aa9
Nissan Hanina <1410797+nissoh@users.noreply.github.com>
2023-07-15 00:12:34 +0300

    remvoe deprecetaed dep indexer

--- browser-indexer/src/index.ts
+++
@@ -1,8 +0,0 @@
-export * from './IndexerState.js'
-
-export { simple_hash } from 'ethereum-indexer'
-export * from './storage/state/OnIndexedDB.js'
-export * from './storage/state/OnLocalStorage.js'
-export * from './storage/stream/OnIndexedDB.js'
-
-export type * from 'ethereum-indexer'
